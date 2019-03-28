b76f5f71fdcc92ab3728d0cc8fad951f462cd6bb
Lucca <brazlucca@gmail.com>
2019-03-28 18:49:39 -0300

    Segundo commit

---
+++ src/TestaComparacaoStrings.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author lbraz
+ *
+ */
+public class TestaComparacaoStrings {
+
+	public static void main(String[] args) {
+
+		String s1 = "paulo";
+		String s2 = "silveira";
+		int resultado = s1.compareTo(s2);
+
+		System.out.println(resultado);
+	}
+}
---
+++ src/TestaListaDeAula.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TestaListaDeAula {
+
+	public static void main(String[] args) {
+
+		Aula a1 = new Aula("Revistando as ArrayLists", 21);
+		Aula a2 = new Aula("Listas de objetos", 20);
+		Aula a3 = new Aula("Relacionamento de listas e objetos", 15);
+
+		ArrayList<Aula> aulas = new ArrayList<>();
+		aulas.add(a1);
+		aulas.add(a2);
+		aulas.add(a3);
+
+		aulas.forEach(aula -> {
+			System.out.println("Percorrendo");
+			System.out.println("Aula: " + aula);
+		});
+
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+//		Collections.sort(aulas);
+		
+		aulas.sort(Comparator.comparing(Aula::getTempo));
+
+		aulas.forEach(aula -> {
+			System.out.println("Percorrendo");
+			System.out.println("Aula: " + aula);
+		});
+	}
+
+}
